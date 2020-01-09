a65d0a860a3aa46b7e128fa9d98d356aa50675e7
shichunfei <378820113@qq.com>
2020-01-09 15:29:33 +0800

    增加主界面

---
+++ app/src/main/java/com/sherlockshi/webactivitybestpractise/MainActivity.java
@@ -0,0 +1,12 @@
+package com.sherlockshi.webactivitybestpractise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Author:      SherlockShi
+ * Email:       sherlock_shi@163.com
+ * Date:        2020-01-09 15:27
+ * Description:
+ */
+public class MainActivity extends AppCompatActivity {
+}
