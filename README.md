0ab37bc918fc03e614f89978002309fce1886cf1
shichunfei <378820113@qq.com>
2020-01-09 14:48:42 +0800

    上传初始项目

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebActivity 最佳实践
+本实践基于 [AgentWeb](https://github.com/Justson/AgentWeb)
---
+++ app/src/main/java/com/sherlockshi/webactivitybestpractise/WebActivity.java
@@ -0,0 +1,14 @@
+package com.sherlockshi.webactivitybestpractise;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+    }
+}
